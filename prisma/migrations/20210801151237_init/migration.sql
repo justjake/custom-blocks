a605ff5ae024ec55e66ceac1c9dcaa64a2517d7d
Jake Teton-Landis <just.1.jake@gmail.com>
2021-08-01 08:25:57 -0700

    create a migration and run it with psql directly

---
+++ prisma/migrations/20210801151237_init/migration.sql
@@ -0,0 +1,44 @@
+-- CreateEnum
+CREATE TYPE "BlockAction" AS ENUM ('Read', 'WriteState', 'Write');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" UUID NOT NULL REFERENCES auth.users,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "NotionOauthToken" (
+    "id" UUID NOT NULL,
+    "userId" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "access_token" TEXT NOT NULL,
+    "workspace_name" TEXT NOT NULL,
+    "workspace_icon" TEXT NOT NULL,
+    "bot_id" TEXT NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CustomBlock" (
+    "id" UUID NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdByUserId" UUID NOT NULL,
+    "State" BYTEA NOT NULL,
+    "publicId" UUID NOT NULL,
+    "publicAccess" "BlockAction",
+
+    PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "NotionOauthToken" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CustomBlock" ADD FOREIGN KEY ("createdByUserId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
