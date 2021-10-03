2692b9a435b281b39310d9db63ed720f345167b8
Jake Teton-Landis <just.1.jake@gmail.com>
2021-10-03 10:52:33 -0700

    fill in profile url

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,12 @@
-import NextAuth from 'next-auth'
-import { DATABASE_URL, NOTION_OAUTH_CLIENT_ID, NOTION_OAUTH_CLIENT_SECRET } from '../../../lib/config'
+import NextAuth from "next-auth";
+import {
+  DATABASE_URL,
+  NOTION_OAUTH_CLIENT_ID,
+  NOTION_OAUTH_CLIENT_SECRET,
+} from "../../../lib/config";
 
 function base64(string: string): string {
-  return Buffer.from(string).toString("base64")
+  return Buffer.from(string).toString("base64");
 }
 
 export default NextAuth({
@@ -10,35 +14,40 @@ export default NextAuth({
   providers: [
     {
       // Seems OK.
-      id: 'notion',
-      name: 'Notion',
+      id: "notion",
+      name: "Notion",
       type: "oauth",
-      version: '2.0',
+      version: "2.0",
       params: {
-        grant_type: "authorization_code"
+        grant_type: "authorization_code",
       },
       accessTokenUrl: "https://api.notion.com/v1/oauth/token",
-      authorizationUrl: "https://api.notion.com/v1/oauth/authorize?response_type=code",
+      authorizationUrl:
+        "https://api.notion.com/v1/oauth/authorize?response_type=code&owner=user",
       clientId: NOTION_OAUTH_CLIENT_ID.string(),
       clientSecret: NOTION_OAUTH_CLIENT_SECRET.string(),
       headers: {
         "Notion-Version": "2021-05-13",
-        Authorization: `Basic ${base64(NOTION_OAUTH_CLIENT_ID.string() + ':' + NOTION_OAUTH_CLIENT_SECRET.string())}`
+        Authorization: `Basic ${base64(
+          NOTION_OAUTH_CLIENT_ID.string() +
+            ":" +
+            NOTION_OAUTH_CLIENT_SECRET.string()
+        )}`,
       },
       // Seems sus.
-      scope: '',
+      scope: "",
       // The profile stuff is broken. We need to fetch a valid JSON document
       // from `profileUrl`; but Notion doesn't offer a /me style endpoint yet.
-      profileUrl: 'https://example.com', 
+      profileUrl: "https://api.notion.com/v1/users/me",
       async profile(profile, tokens) {
         return {
           // TODO
-          id: '1'
-        }
+          id: "1",
+        };
       },
-    }
+    },
   ],
 
   // A database is optional, but required to persist accounts in a database
-  database: DATABASE_URL
-})
+  database: DATABASE_URL,
+});
