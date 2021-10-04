0569973fd9235eb7ad0b1874a0882f3925d6daeb
Jake Teton-Landis <just.1.jake@gmail.com>
2021-10-04 08:29:25 -0700

    can now actually auth as user, but cant save to db

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Provider } from 'next-auth/client'
-
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Provider } from "next-auth/client";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider session={pageProps.session}>
       <Component {...pageProps} />
     </Provider>
-  )
+  );
 }
-export default MyApp
+export default MyApp;
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,9 @@
-import NextAuth from "next-auth";
+import { GetUserResponse } from "@notionhq/client/build/src/api-endpoints";
+import NextAuth, { Profile } from "next-auth";
 import {
-  DATABASE_URL,
   NOTION_OAUTH_CLIENT_ID,
   NOTION_OAUTH_CLIENT_SECRET,
+  PGBOUNCER_DATABASE_URL,
 } from "../../../lib/config";
 
 function base64(string: string): string {
@@ -27,7 +28,7 @@ export default NextAuth({
       clientId: NOTION_OAUTH_CLIENT_ID.string(),
       clientSecret: NOTION_OAUTH_CLIENT_SECRET.string(),
       headers: {
-        "Notion-Version": "2021-05-13",
+        "Notion-Version": "2021-08-16",
         Authorization: `Basic ${base64(
           NOTION_OAUTH_CLIENT_ID.string() +
             ":" +
@@ -36,18 +37,49 @@ export default NextAuth({
       },
       // Seems sus.
       scope: "",
-      // The profile stuff is broken. We need to fetch a valid JSON document
-      // from `profileUrl`; but Notion doesn't offer a /me style endpoint yet.
       profileUrl: "https://api.notion.com/v1/users/me",
       async profile(profile, tokens) {
-        return {
-          // TODO
-          id: "1",
-        };
+        return userProfile(profile as any as GetUserResponse);
       },
     },
   ],
 
   // A database is optional, but required to persist accounts in a database
-  database: DATABASE_URL,
+  database: PGBOUNCER_DATABASE_URL.string(),
 });
+
+type NotionBotOwner =
+  | {
+      type: "workspace";
+    }
+  | {
+      type: "user";
+      user: GetUserResponse & { type: "person" };
+    };
+
+type Person = Extract<GetUserResponse, { type: "person" }>;
+
+function getUserPerson(user: GetUserResponse): Person {
+  if (user.type === "bot") {
+    // Return the profile of the owner
+    // Type is invalid as of 0.4.0, see https://github.com/makenotion/notion-sdk-js/issues/205
+    const bot = user.bot as any as { owner: NotionBotOwner };
+    if (bot.owner.type === "workspace") {
+      throw new Error("Workspace bots dont have a user profile");
+    }
+    return getUserPerson(bot.owner.user);
+  }
+
+  return user;
+}
+
+function userProfile(user: GetUserResponse): Profile & { id: string } {
+  const person = getUserPerson(user);
+
+  return {
+    id: person.id,
+    name: person.name || undefined,
+    email: person.person.email,
+    image: person.avatar_url || undefined,
+  };
+}
